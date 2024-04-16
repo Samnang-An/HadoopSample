de2c3fd36b412707cf5382a17956cf8d37f3c19e
samnang-an <samnang.an7@gmail.com>
2024-04-16 15:09:06 -0500

    fix: find average issue
    - check valid ip address

--- src/main/java/com/bd/lab/average/AverageReducer.java
+++ src/main/java/com/bd/lab/average/AverageReducer.java
@@ -2,17 +2,17 @@ package com.bd.lab.average;
 
 import java.io.IOException;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WCAverageReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+public class AverageReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 
   private final DoubleWritable result = new DoubleWritable();
 
+  @Override
   public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
       throws IOException, InterruptedException {
-    int sum = 0, count = 0;
+    double sum = 0, count = 0;
     for (DoubleWritable val : values) {
       sum += val.get();
       count += 1;
