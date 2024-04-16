7f73d39017a0dc7f7c38f0e9116cda02d51fe790
samnang-an <samnang.an7@gmail.com>
2024-04-16 10:20:22 -0500

    feat: add run script for find average

---
+++ scripts/average-filesize.sh
@@ -0,0 +1,8 @@
+# Cleanup directories
+hadoop fs -rm -r -f /average-output
+hadoop fs -rm -r -f /average
+# Create input directory and add text file
+hadoop fs -mkdir /average
+hadoop fs -put files/logs/access_log /average
+# Run MapReduce Program
+ hadoop jar target/WordCount-1.0-SNAPSHOT.jar com.bd.lab.average.WCAverageRunner /average/access_log /average-output
