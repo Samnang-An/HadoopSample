7cbdf42fd302d32a3498b79b9817e63143627f00
samnang-an <samnang.an7@gmail.com>
2024-04-17 10:32:05 -0500

    feat: add output result

--- scripts/inmapper-average-filesize.sh
+++ scripts/inmapper-average-filesize.sh
@@ -3,6 +3,6 @@ hadoop fs -rm -r -f /inmapper-average-output
 hadoop fs -rm -r -f /inmapper-average
 # Create input directory and add text file
 hadoop fs -mkdir /inmapper-average
-hadoop fs -put files/logs/access_log /inmapper-average
+hadoop fs -put files/input/access_log /inmapper-average
 # Run MapReduce Program
 hadoop jar target/WordCount-1.0-SNAPSHOT.jar com.bd.lab.inmapperaverage.InMapperAverageRunner /inmapper-average/access_log /inmapper-average-output
--- scripts/inmapper-wordcount.sh
+++ scripts/inmapper-wordcount.sh
@@ -3,6 +3,6 @@ hadoop fs -rm -r -f /inmapper-wordcount-output
 hadoop fs -rm -r -f /inmapper-wordcount
 # Create input directory and add text file
 hadoop fs -mkdir /inmapper-wordcount
-hadoop fs -put files/big.txt /inmapper-wordcount
+hadoop fs -put files/input/big.txt /inmapper-wordcount
 # Run MapReduce Program
  hadoop jar target/WordCount-1.0-SNAPSHOT.jar com.bd.lab.inmapperwordcount.WCInMapperRunner /inmapper-wordcount/big.txt /inmapper-wordcount-output
